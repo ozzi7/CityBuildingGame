8572d2e26829795a16dfa2ed1f48d92db512f722
Willyfisch <willyfisch@yahoo.com>
2018-02-21 21:47:58 +0100

    bug fix in terrain.cpp creatgeometry

--- CityBuildingGame/terrain.cpp
+++ CityBuildingGame/terrain.cpp
@@ -157,15 +157,11 @@ void Terrain::CreateGeometry(int startX, int endX, int startY, int endY)
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices[0], GL_STATIC_DRAW);
 
 	// position attribute, 5th attribute can be 0 for tightly packed, its equal to 3*sizeof(float)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8* sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// normals attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
 	// texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 }
 
