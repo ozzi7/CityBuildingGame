06f505fa0987c249b91d799de2fb00f1255f85f8
ozzi7 <nielregen@gmail.com>
2019-02-24 16:59:52 +0100

    Fixed misplaced object.cpp

--- CityBuildingGame/gameObjects/object.cpp
+++ CityBuildingGame/gameObjects/object.cpp
@@ -1,10 +1,21 @@
 #pragma once
-#include "object.h"
+#include "gameObjects/object.h"
 
-Object::Object(glm::vec3 aPosition, glm::vec3 aScale, float aRotation)
+Object::Object(glm::vec3 aPosition, glm::vec3 aScale, glm::vec3 aRotation)
 {
 	position = aPosition;
 	scale = aScale;
 	rotation = aRotation;
+	recalculateModelMatix();
 }
 Object::Object() {};
+
+void Object::recalculateModelMatix()
+{
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, position);
+	model = glm::scale(model, scale);
+	model = glm::rotate(model, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::rotate(model, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+}
