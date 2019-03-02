2317edf4601675468d9f457a1b0d91ebe2678386
Nico <willyfisch@yahoo.com>
2019-03-02 16:49:40 +0100

    #23 instanced classes complete

--- CityBuildingGame/globals.h
+++ CityBuildingGame/globals.h
@@ -12,7 +12,7 @@ const unsigned int MAP_HEIGHT = 400;
 const float FIR_DENSITY = 5.0f;		// ~ 0 - 10
 const float HILL_HEIGHT = 10.0f;	// ~ 0 - 10
 
-const float SCROLL_SPEED = 1.0f;
+const float SCROLL_SPEED = 5.0f;
 const float VISIBLE_RANGE = 1.4f;	// ~1.6 is entire screen
 const float ZOOM_DEFAULT = 10.0f;
 
--- CityBuildingGame/rendering/instanced_mesh.cpp
+++ CityBuildingGame/rendering/instanced_mesh.cpp
@@ -9,40 +9,21 @@ InstancedMesh::InstancedMesh(std::vector<Vertex> vertices, std::vector<unsigned 
 	this->textures = textures;
 
 	setupMesh();
+	setupInstancedMesh();
 }
 
-void InstancedMesh::setupMesh()
+void InstancedMesh::setupInstancedMesh()
 {
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);	
 	glGenBuffers(1, &instanceVBO);
 
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-
-	glEnableVertexAttribArray(2);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
-	
-
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * textures.size(), textures.data(), GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(3);
 	glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(glm::vec4), (void*)0);
 	glEnableVertexAttribArray(4);
-	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(glm::vec4), (void*)(1 * sizeof(glm::vec4)));
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(glm::vec4), (void*)sizeof(glm::vec4));
 	glEnableVertexAttribArray(5);
 	glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(glm::vec4), (void*)(2 * sizeof(glm::vec4)));
 	glEnableVertexAttribArray(6);
@@ -56,7 +37,6 @@ void InstancedMesh::setupMesh()
 	glBindVertexArray(0);
 }
 
-// render the mesh
 void InstancedMesh::Draw(int instances)
 {
 	glDrawElementsInstanced(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, 0, instances);
@@ -68,33 +48,9 @@ void InstancedMesh::Draw(int instances)
 
 void InstancedMesh::Bind(Shader& shader, std::vector<glm::mat4>& models)
 {
+	Mesh::Bind(shader);
+
 	glBindVertexArray(VAO);
-
-	// bind appropriate textures
-	unsigned int diffuseNr = 1;
-	unsigned int specularNr = 1;
-	unsigned int normalNr = 1;
-	unsigned int heightNr = 1;
-	for (unsigned int i = 0; i < textures.size(); i++)
-	{
-		glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
-										  // retrieve texture number (the N in diffuse_textureN)
-		std::string number;
-		std::string name = textures[i].type;
-		if (name == "texture_diffuse")
-			number = std::to_string(diffuseNr++);
-		else if (name == "texture_specular")
-			number = std::to_string(specularNr++); // transfer unsigned int to stream
-		else if (name == "texture_normal")
-			number = std::to_string(normalNr++); // transfer unsigned int to stream
-		else if (name == "texture_height")
-			number = std::to_string(heightNr++); // transfer unsigned int to stream
-
-												 // now set the sampler to the correct texture unit
-		glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
-		// and finally bind the texture
-		glBindTexture(GL_TEXTURE_2D, textures[i].id);
-	}
 
 	// bind per instance data
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
